05d68bef7f5d351a6ac948647d49c532cf1730f3
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-08-06 16:40:28 +0530

    Update gcd-of-two-numbers.py

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.py
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.py
@@ -4,3 +4,21 @@ class Solution:
             return a
 
         return self.gcd(b, a % b)
+
+""" 
+class Solution:
+    def gcd(self, a, b):
+        if a==0 or b==0:
+            return min(a, b)
+            
+        if a>=b:
+            if a%b==0:
+                return b
+            return self.gcd(a-b, b)
+        if b>a:
+            if b%a==0:
+                return a
+            return self.gcd(a, b-a)
+            
+                
+"""

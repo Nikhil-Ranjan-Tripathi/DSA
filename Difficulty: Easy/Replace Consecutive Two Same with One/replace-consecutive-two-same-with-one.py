b24c249ee91ef9386f34fd01e72d8000c036c9fe
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-08-08 03:09:38 +0530

    Update replace-consecutive-two-same-with-one.py

--- Difficulty: Easy/Replace Consecutive Two Same with One/replace-consecutive-two-same-with-one.py
+++ Difficulty: Easy/Replace Consecutive Two Same with One/replace-consecutive-two-same-with-one.py
@@ -2,18 +2,29 @@ class Solution:
     def removeDuplicates(self, s):
         if not s:
             return ""
-
         ans = [s[0]]
-
         def solve(i):
             if i == len(s):
                 return
-
             if s[i] != ans[-1]:
                 ans.append(s[i])
-
             solve(i + 1)
-
         solve(1)
 
         return "".join(ans)
+
+"""
+class Solution:
+    def removeDuplicates(self, s):
+        ans = s[0]
+        def solve(n, ans):
+            if n==len(s):
+                return ans
+            
+            if s[n]!=ans[-1]:
+                ans+=s[n]
+            return solve(n+1, ans)
+                
+        
+        return solve(1, ans)
+"""

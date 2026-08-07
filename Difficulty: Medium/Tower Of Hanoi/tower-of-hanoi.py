9041f03c8617686ad4e359fd2a0306c431e790cf
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-08-07 21:05:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.py
@@ -0,0 +1,10 @@
+class Solution:
+    def towerOfHanoi(self, n, fromm, to, aux):
+        if n == 1:
+            return 1
+
+        return (
+            self.towerOfHanoi(n - 1, fromm, aux, to)
+            + 1
+            + self.towerOfHanoi(n - 1, aux, to, fromm)
+        )

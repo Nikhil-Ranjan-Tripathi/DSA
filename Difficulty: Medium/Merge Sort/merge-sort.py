d01392e0100c6e3bffab6c8ed917487c04690cec
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-08-14 20:29:38 +0530

    Update merge-sort.py

--- Difficulty: Medium/Merge Sort/merge-sort.py
+++ Difficulty: Medium/Merge Sort/merge-sort.py
@@ -1,19 +1,14 @@
 class Solution:
     def mergeSort(self, arr, l, r):
-        # Base case
         if l >= r:
             return
 
-        # Find middle
         mid = (l + r) // 2
 
-        # Sort left half
         self.mergeSort(arr, l, mid)
 
-            # Sort right half
         self.mergeSort(arr, mid + 1, r)
 
-            # Merge both halves
         self.merge(arr, l, mid, r)
 
 
@@ -26,7 +21,6 @@ class Solution:
         j = 0
         k = l
 
-        # Compare elements from left and right
         while i < len(left) and j < len(right):
     
             if left[i] <= right[j]:
@@ -37,13 +31,11 @@ class Solution:
                 j += 1
             k += 1
 
-        # Remaining elements in left
         while i < len(left):
             arr[k] = left[i]
             i += 1
             k += 1
 
-        # Remaining elements in right
         while j < len(right):
             arr[k] = right[j]
             j += 1

ac678e89ea5f7372c5421a5edde55d52c7245994
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-08-14 23:56:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.py
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.py
@@ -1,12 +1,5 @@
 class Solution:
     def deleteMid(self, s):
-        def solve(k):
-            if k == 0:
-                s.pop()
-                return
-
-            x = s.pop()
-            solve(k - 1)
-            s.append(x)
-
-        solve(len(s) // 2)
+        if len(s)//2==0:
+            return 0
+        s.pop((len(s)-1)//2)

2811b0b398943de0c237c541c8f8741d2af3a608
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-01 17:12:03 +0530

    Time: 3 ms (74.93%) | Memory: 19.4 MB (12.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,36 +3,37 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         if not grid:
             return 0
-        
+
         rows, cols = len(grid), len(grid[0])
+        fresh = 0
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
         que = deque()
-        fresh_count = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]==2:
-                    que.append((r,c))
                 if grid[r][c]==1:
-                    fresh_count += 1
-        if fresh_count==0:
+                    fresh+=1
+                elif grid[r][c]==2:
+                    que.append((r,c))
+
+        if fresh==0:
             return 0
         
         mins = 0
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
-        
-        while que:
-            if fresh_count==0:
-                return mins
-            mins+=1
-            level_size = len(que)
-            for _ in range(level_size):
-                r, c = que.popleft()
-                for dr, dc in directions:
-                    nr = r+dr
-                    nc = c+dc
-                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
-                        grid[nr][nc]=2
-                        fresh_count-=1
-                        que.append((nr,nc))
 
-        return -1 if fresh_count>0 else mins
-        
+        while que and fresh>0:
+
+            for _ in range(len(que)):
+                rr, cc = que.popleft()
+
+                for dr, dc in directions:
+                    nr = rr+dr
+                    nc = cc+dc
+
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
+                        grid[nr][nc] = 2
+                        fresh-=1
+                        que.append((nr, nc))
+                
+            mins+=1
+
+        return -1 if fresh>0 else mins

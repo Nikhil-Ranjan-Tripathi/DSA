cf8c110697c9a25caa0204d261b017a2f3630dd4
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-07 00:19:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -0,0 +1,10 @@
+class Solution:
+    def knapsack(self, W: int, val: list[int], wt: list[int]) -> int:
+        n = len(val)
+        dp = [0] * (W + 1)
+
+        for i in range(n):
+            for w in range(W, wt[i] - 1, -1):
+                dp[w] = max(dp[w], val[i] + dp[w - wt[i]])
+
+        return dp[W]

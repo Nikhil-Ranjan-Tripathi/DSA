e79462762ff22fc90186bba9a157f5cb6beab750
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-07 20:39:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.py
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.py
@@ -1,33 +1,32 @@
 class Solution:
     def ratInMaze(self, maze: list[list[int]]) -> list[str]:
-        # code here
         m = len(maze)
         
-        paths = []
+        results = []
         
-        visited = [[False]*m for _ in range(m)]
+        visi = [[False]*m for _ in range(m)]
         
         def is_safe(r,c):
-            return (0<=r<m and 0<=c<m and maze[r][c]==1 and visited[r][c]==False)
-                
-        
-        def dfs(r,c,path):
+            return (0<=r<m and 0<=c<m and maze[r][c]==1 and visi[r][c]==False)
+            
+        def dfs(r,c,p):
             if not is_safe(r,c):
                 return
             
             if r==m-1 and c==m-1:
-                paths.append(path)
-                return
+                results.append(p)
+                
+            visi[r][c]=True
             
-            visited[r][c]=True
+            dfs(r+1,c,p+"D")
+            dfs(r,c-1,p+"L")
+            dfs(r,c+1,p+"R")
+            dfs(r-1,c,p+"U")
             
-            dfs(r+1,c,path+'D')
-            dfs(r, c-1, path +'L')
-            dfs(r,c+1,path+'R')
-            dfs(r-1,c,path+'U')
             
-            visited[r][c]=False
+            
+            visi[r][c]=False
             
         dfs(0,0,'')
         
-        return paths
+        return results

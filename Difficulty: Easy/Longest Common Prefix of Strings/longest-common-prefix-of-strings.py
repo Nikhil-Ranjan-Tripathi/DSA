ab362873e8f93785d85154e5d0e24b0fb5537df1
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-10 15:11:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.py
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.py
@@ -1,17 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, arr):
-        a = ''
-        b = arr[0]
-        for i in range(len(arr)):
-            j=0
-            while j<len(b) and j<len(arr[i]):
-                if b[j]!=arr[i][j]:
-                    break
-                j+=1
-                
-            b = b[:j]
-                
-            if b[:j]=="":
-                return ""
-                
-        return b
+        a = []
+        n = min(arr, key=len)
+        j=0
+        for i in range(len(n)):
+            ch = arr[0][i]
+            
+            for s in arr:
+                if s[i]!=ch:
+                    return ''.join(a)
+            
+            a.append(ch)
+        
+        return ''.join(a)

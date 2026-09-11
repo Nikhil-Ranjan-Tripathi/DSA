c5b7a82bbf306d9a83363e3b8c24fb0c8ff4c0ff
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-11 20:44:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+	def mergeOverlap(self, arr):
+		# Code here
+		arr.sort()
+		res = [arr[0]]
+		
+		for i in range(1, len(arr)):
+		    last = res[-1]
+		    curr =arr[i]
+		    
+		    if curr[0]<=last[1]:
+		        last[1] = max(last[1], curr[1])
+		    else:
+		        res.append(curr)
+		        
+		return res

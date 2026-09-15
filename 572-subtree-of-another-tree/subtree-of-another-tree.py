9de8c812770c749742c3a31f26e2dc6bd40e3e7f
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-16 00:43:56 +0530

    Update subtree-of-another-tree.py

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -27,3 +27,23 @@ class Solution:
             return search(node.left) or search(node.right)
 
         return search(root)
+
+
+"""
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode],
+                  subRoot: Optional[TreeNode]) -> bool:
+
+        def serialize(node):
+            if not node:
+                return ",#"
+
+            return ("," + str(node.val) +
+                    serialize(node.left) +
+                    serialize(node.right))
+
+        root_str = serialize(root)
+        sub_str = serialize(subRoot)
+
+        return sub_str in root_str
+"""

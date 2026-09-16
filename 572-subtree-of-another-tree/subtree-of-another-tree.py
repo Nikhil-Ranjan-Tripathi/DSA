d6e2b50452a0212a0e1a21dbaae57807939bd731
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-16 18:04:47 +0530

    Time: 43 ms (44.07%) | Memory: 19.9 MB (40.05%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -28,22 +28,15 @@ class Solution:
 
         return search(root)
 
-
 """
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode],
-                  subRoot: Optional[TreeNode]) -> bool:
+def stringer(root):
+    if not root:
+        return ",#"
 
-        def serialize(node):
-            if not node:
-                return ",#"
+    return (str(root.val)+","+stringer(root.left)+stringer(root.right))
 
-            return ("," + str(node.val) +
-                    serialize(node.left) +
-                    serialize(node.right))
+    main = stringer(root)
+    sub = stringer(subRoot)
 
-        root_str = serialize(root)
-        sub_str = serialize(subRoot)
-
-        return sub_str in root_str
+    return sub in main
 """

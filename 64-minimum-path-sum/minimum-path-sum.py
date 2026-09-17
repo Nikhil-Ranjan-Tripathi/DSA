82e3ea5d06095c6cca331528ac79cbcb221394d6
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-18 02:13:56 +0530

    Update minimum-path-sum.py

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -17,3 +17,47 @@ class Solution:
                 dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
 
         return dp[m-1][n-1]
+
+"""
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [0] * n
+        
+        #initial states
+        dp[0] = grid[0][0]
+        for i in range(1, n):
+            dp[i] = dp[i-1] + grid[0][i]
+        
+
+        for i in range(1, m):
+            dp[0] = dp[0] + grid[i][0]
+            for j in range(1, n):
+                dp[j] = min(dp[j], dp[j-1]) + grid[i][j]
+        return dp[-1]
+        
+"""
+
+"""
+def solve(grid):
+    m = len(grid)
+    n = len(grid[0])
+    
+    def is_safe(a, b):
+        return (0<=a<m and 0<=b<n)
+    
+    def solving(a, b, total):
+        
+        if not is_safe(a, b):
+            return float('inf')
+            
+        total+=grid[a][b]
+        
+        if a==m-1 and b==n-1:
+            return total
+        
+        return min(solving(a+1, b, total), solving(a, b+1, total))
+    
+    return solving(0,0, 0)
+"""

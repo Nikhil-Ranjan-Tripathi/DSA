24f45fcadebf127921dac83b9aa3fb0770ac8171
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-19 15:40:13 +0530

    Update subset-sum-problem.py

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
@@ -8,3 +8,29 @@ class Solution:
                 dp[s] = dp[s] or dp[s - num]
                 
         return dp[sum]
+
+"""
+class Solution:
+    def isSubsetSum(self, arr: list[int], sum: int) -> bool:
+        # code here
+        memo = {}
+        
+        def solve(i, left):
+            if left==0:
+                return True
+            
+            if i>=len(arr) or left<0:
+                return False
+            
+            if (i,left) in memo:
+                return memo[(i,left)]
+                
+            take = solve(i+1, left-arr[i])
+            skip = solve(i+1, left)
+            
+            memo[(i,left)] = take or skip
+            
+            return memo[(i, left)]
+            
+        return solve(0, sum)
+"""

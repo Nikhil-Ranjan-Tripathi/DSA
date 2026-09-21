30558f43157598339cfb0095b2c9f9aa639f9861
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-22 01:04:44 +0530

    Time: 337 ms (76.61%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -3,3 +3,8 @@ select class from(select class, Count(*) as temp
 from Courses c 
 group by class
 having temp>=5)t
+
+-- SELECT class
+-- FROM Courses
+-- GROUP BY class
+-- HAVING COUNT(DISTINCT student) >= 5;

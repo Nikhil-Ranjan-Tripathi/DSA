8b380cc024677477dc8288b5a580f81ecf6ab2ee
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-23 19:35:11 +0530

    Time: 746 ms (89.17%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -7,4 +7,5 @@ WHERE REGEXP_like(mail, '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$', 'c');
 /*
 "^" marks the start of the string, without ^, MySQL can find a matching portion somewhere inside the email. Similarly "$", marks the end of the string. "*" means the there can be 0 to multiple characters of the following type.
 In REGEXP, '.' means “any single character”, while '\.' means a literal dot '.'
+'c' is for stating the case-senstivity of the string.
 */

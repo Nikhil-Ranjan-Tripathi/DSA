08e2f80426a51c800031406b435ed3c194d0cce0
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-23 19:45:02 +0530

    Time: 412 ms (82.44%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,3 +1,15 @@
 # Write your MySQL query statement below
 select patient_id, patient_name, conditions from Patients
 where conditions Regexp '(^| )DIAB1'
+
+/*
+(^| ) means the DIAB1 is either at the beginning or there is a space prefix to DIAB1.
+also we can get the same result by
+
+
+SELECT patient_id,patient_name,conditions
+from Patients 
+where conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
+
+
+*/

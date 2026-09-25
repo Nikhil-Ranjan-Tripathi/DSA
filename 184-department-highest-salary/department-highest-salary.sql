8845f864f84514dfc08016d5d897a0808ad81c7e
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-09-25 17:04:04 +0530

    Time: 1071 ms (31.01%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -5,3 +5,22 @@ where Salary = (
     select max(e2.salary) from Employee e2
     where e2.departmentId = e.departmentId
 )
+
+-- SELECT
+--     Department,
+--     Employee,
+--     Salary
+-- FROM (
+--     SELECT
+--         d.name AS Department,
+--         e.name AS Employee,
+--         e.salary AS Salary,
+--         DENSE_RANK() OVER (
+--             PARTITION BY e.departmentId
+--             ORDER BY e.salary DESC
+--         ) AS rnk
+--     FROM Employee e
+--     JOIN Department d
+--         ON e.departmentId = d.id
+-- ) t
+-- WHERE rnk = 1;
